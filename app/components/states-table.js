38489668a3c219d362cc6a996e5132e131e9e639
Matt Chastain <matthewdchastain@gmail.com>
2015-06-24 08:16:44 -0400

    Basic structure; normal mode finished

--- app/components/states-table.js
+++ app/components/states-table.js
@@ -3,6 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
 	tagName: 'div',
-	classNames: 'states-list-item'
+	classNames: 'states-list-item',
+
+	actions: {
+		sortStates: function(places) {
+			places.sort(function(a,b) {
+				if (a.state === "Alabama") {
+					if (a.name > b.name) {
+						return a;
+					}
+				} else {
+					if (a.name > b.name) {
+						return b;
+					}
+				}
+			});
+			places.forEach(function(state) {
+				console.log(state.state);
+			});
+		}
+	}
 
 });
